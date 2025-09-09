71b44f4516b5a3df273906f2b784963f2b6a39fa
mayazhrij <azhjasmine@gmail.com>
2025-09-09 16:48:30 +0700

    edited params in route

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,20 +1,10 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prismadb";
 import { Category } from "@prisma/client"; // optional, for type hinting
 
-export async function GET(
-    req: NextRequest,
-    context: { params: Promise<{ catName: string }> }
-): Promise<Response> {
+export async function GET() {
     try {
-        const { catName } = await context.params;
-
-        const categories: Category[] = await prisma.category.findMany({
-            where: {
-                catName
-            },
-        });
-
+        const categories = await prisma.category.findMany();
         return NextResponse.json(categories, { status: 200 });
     } catch (error) {
         console.error("Error fetching categories:", error);
