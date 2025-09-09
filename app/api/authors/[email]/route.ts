7438e44032bc98b9aef0cecdb42405efc1a519a1
mayazhrij <azhjasmine@gmail.com>
2025-09-09 14:24:48 +0700

    Update route.ts

--- app/api/authors/[email]/route.ts
+++ app/api/authors/[email]/route.ts
@@ -2,11 +2,12 @@ import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prismadb";
 
 
+
+
 export async function GET(req: Request,
-    { params }: { params: { email: string } }
-) {
+    { params }: { params: Promise<{ email: string }> }) {
     try {
-        const email = params.email
+        const {email}  = await params;
         const posts = await prisma.user.findUnique({
             where: { email },
             include: {
